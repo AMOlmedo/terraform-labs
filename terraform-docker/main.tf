18656d012ef38786e7e40389a4dbe38e4ab37667
Adrian Olmedo <adrian.olmedo.1973@gmail.com>
2026-06-25 00:41:42 +0000

    main.tf docker provider, create lab into infra

---
+++ terraform-docker/main.tf
@@ -0,0 +1,42 @@
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:24.04"
+}
+resource "docker_network" "ansible_net" {
+  name = "ansible-net"
+}
+resource "docker_container" "infra" {
+  name  = "infra"
+  image = docker_image.ubuntu.image_id
+  command = ["sleep", "infinity"]
+
+  networks_advanced {
+    name = docker_network.ansible_net.name
+  }
+}
+resource "docker_container" "node1" {
+  name  = "node1"
+  image = docker_image.ubuntu.image_id
+  command = ["sleep", "infinity"]
+
+  networks_advanced {
+    name = docker_network.ansible_net.name
+  }
+}
+resource "docker_container" "node2" {
+  name  = "node2"
+  image = docker_image.ubuntu.image_id
+  command = ["sleep", "infinity"]
+
+  networks_advanced {
+    name = docker_network.ansible_net.name
+  }
+}
+resource "docker_container" "node3" {
+  name  = "node3"
+  image = docker_image.ubuntu.image_id
+  command = ["sleep", "infinity"]
+
+  networks_advanced {
+    name = docker_network.ansible_net.name
+  }
+}
